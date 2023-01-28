ea4748f2d714cf4adfda07479ec05552a3496c5e
Denis Beliakov <ddenn@outlook.com>
2023-01-28 04:32:58 +0300

    Спринт 8/14: 8 → Тема 2/5: Инициализация второго проекта → Урок 4/4

---
+++ ImageFeed/Helpers/UIColors+Extenshions.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static var ypBlue: UIColor {UIColor(named: "blue")!}
+    static var ypRed: UIColor {UIColor(named: "red")!}
+    static var ypBlack: UIColor {UIColor(named: "black")!}
+    static var ypBackground: UIColor {UIColor(named: "background")!}
+    static var ypWhite: UIColor {UIColor(named: "white")!}
+    static var ypWhiteAlpha: UIColor {UIColor(named: "whiteAlpha")!}
+    static var ypGray: UIColor {UIColor(named: "gray")!}
+}
