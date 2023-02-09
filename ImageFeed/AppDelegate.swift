34e7ab5f2ff1baf4ee689bf01e694cb34a2a38c9
Denis Beliakov <ddenn@outlook.com>
2023-02-09 14:49:31 +0300

    мелкие правки

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -4,7 +4,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         sleep(3)
@@ -25,6 +24,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
