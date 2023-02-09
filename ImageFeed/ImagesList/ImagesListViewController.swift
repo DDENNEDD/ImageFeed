86f46870949fa824b67a4b5975a43e6f17563b29
Denis Beliakov <ddenn@outlook.com>
2023-02-09 18:33:50 +0300

    мелкие правки

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -12,12 +12,8 @@ class ImagesListViewController: UIViewController {
         }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-
     }
-
-    func configCell(for cell: ImagesListCell) { }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
