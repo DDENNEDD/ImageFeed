040a436b9181c9237f6726d7c2c5d2c1322efbd2
Denis Beliakov <ddenn@outlook.com>
2023-02-10 20:20:36 +0300

    правки в соответствии с заданиями урока

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet var avatarImage: UIImageView!
+    @IBOutlet var logoutButton: UIButton!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginLabel: UILabel!
+
+    @IBAction func didTapLogoutButton() {
+    }
+}
