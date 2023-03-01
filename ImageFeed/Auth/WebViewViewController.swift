f616d7fba0fdfb7ddf85cda2a0e07c6f64fe79eb
Denis Beliakov <ddenn@outlook.com>
2023-03-02 00:58:32 +0300

    правки в соответствием с заданиями урока

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -5,4 +5,16 @@ final class WebViewViewController: UIViewController {
     @IBOutlet var webView: WKWebView!
     @IBAction func didTapBackButton(_ sender: Any?) {
     }
+    override func viewDidLoad() {
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: AccessKey),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
