62b2f2440f658498755df29fbe71316809d1a527
Denis Beliakov <ddenn@outlook.com>
2023-03-02 01:09:28 +0300

    правки в соответствии с заданиями урока

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -16,5 +16,34 @@ final class WebViewViewController: UIViewController {
         let url = urlComponents.url!
         let request = URLRequest(url: url)
         webView.load(request)
+        webView.navigationDelegate = self
+    }
+}
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+         if let code = code(from: navigationAction) {
+                decisionHandler(.cancel)
+          } else {
+                decisionHandler(.allow)
+            }
+    }
+
+}
+
+private func code(from navigationAction: WKNavigationAction) -> String? {
+    if
+        let url = navigationAction.request.url,
+        let urlComponents = URLComponents(string: url.absoluteString),
+        urlComponents.path == "/oauth/authorize/native",
+        let items = urlComponents.queryItems,
+        let codeItem = items.first(where: { $0.name == "code" }) {
+        return codeItem.value
+    } else {
+        return nil
     }
 }
