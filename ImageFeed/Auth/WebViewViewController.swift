284ecc74f25dd94fec4a0b8f48d54ee62da2fbea
Denis Beliakov <ddenn@outlook.com>
2023-03-02 18:29:10 +0300

    правки кода

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -1,27 +1,24 @@
 import WebKit
 import UIKit
 
+private struct APIConstants {
+    static let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    static let code = "code"
+}
+
 final class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
+
     weak var delegate: WebViewViewControllerDelegate?
-    @IBOutlet var webView: WKWebView!
-    @IBAction func didTapBackButton(_ sender: Any?) {
-        delegate?.webViewViewControllerDidCancel(self)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.navigationDelegate = self
-        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: AccessKey),
-            URLQueryItem(name: "redirect_uri", value: RedirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: AccessScope)
-        ]
-        let url = urlComponents.url!
+        loadWebView()
+    }
 
-        let request = URLRequest(url: url)
-        webView.load(request)
+    @IBAction func didTapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
     }
 }
 
@@ -29,26 +26,37 @@ extension WebViewViewController: WKNavigationDelegate {
     func webView(
         _ webView: WKWebView,
         decidePolicyFor navigationAction: WKNavigationAction,
-        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-    ) {
-        if let code = code(from: navigationAction) {
-            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
-            decisionHandler(.cancel)
-        } else {
-            decisionHandler(.allow)
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            if let code = code(from: navigationAction) {
+                delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
         }
+    func loadWebView() {
+        var urlComponents = URLComponents(string: APIConstants.UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: AccessKey),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "response_type", value: APIConstants.code),
+            URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
+}
 
-    private func code(from navigationAction: WKNavigationAction) -> String? {
-        if
-            let url = navigationAction.request.url,
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" }) {
-            return codeItem.value
-        } else {
-            return nil
-        }
+private func code(from navigationAction: WKNavigationAction) -> String? {
+    if
+        let url = navigationAction.request.url,
+        let urlComponents = URLComponents(string: url.absoluteString),
+        urlComponents.path == "/oauth/authorize/native",
+        let items = urlComponents.queryItems,
+        let codeItem = items.first(where: { $0.name == "code" }) {
+        return codeItem.value
+    } else {
+        return nil
     }
 }
--- ImageFeed/Helpers/Constants.swift
+++ ImageFeed/Helpers/Constants.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 // swiftlint:disable all
-let AccessKey = "//WO_3W1D81z4c4MlX09e_Q7gGLV0sB-tYTnKfozhzlig"
-let SecretKey = "//GokulK15gaiE4tvoN_HtqS49gJrTOEcQTy3rM-dg-nQ"
+let AccessKey = "WO_3W1D81z4c4MlX09e_Q7gGLV0sB-tYTnKfozhzlig"
+let SecretKey = "GokulK15gaiE4tvoN_HtqS49gJrTOEcQTy3rM-dg-nQ"
 let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let AccessScope = "public+read_user+write_likes"
 let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
-let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
