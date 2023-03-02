56430ba9a9f9eb5c1b2f1d692e1c91057b4676d7
Denis Beliakov <ddenn@outlook.com>
2023-03-02 18:41:52 +0300

    мелкие правки

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -19,7 +19,6 @@ extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         //TODO: process code
     }
-
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
         dismiss(animated: true)
     }
