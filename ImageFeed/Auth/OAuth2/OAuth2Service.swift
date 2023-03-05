db98d95a99f63d1a83ed243f3a99742e51c87914
Denis Beliakov <ddenn@outlook.com>
2023-03-05 03:27:18 +0300

    мелкие правки

--- ImageFeed/Auth/OAuth2/OAuth2Service.swift
+++ ImageFeed/Auth/OAuth2/OAuth2Service.swift
@@ -2,6 +2,6 @@ import Foundation
 
 final class OAuth2Service {
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
-        completion(.success("")) 
+        completion(.success(""))
     }
 }
