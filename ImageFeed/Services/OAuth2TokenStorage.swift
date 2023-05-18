cae22688fe787af332f6bc4c14ea2e12df58d767
Denis Beliakov <ddenn@outlook.com>
2023-05-18 19:43:04 +0300

    мелкие правки

--- ImageFeed/Services/OAuth2TokenStorage.swift
+++ ImageFeed/Services/OAuth2TokenStorage.swift
@@ -1,16 +1,5 @@
 import Foundation
 
-//final class OAuth2TokenStorage {
-//    var token: String? {
-//        get {
-//            UserDefaults.standard.string(forKey: "bearerToken")
-//        }
-//        set (newToken) {
-//            UserDefaults.standard.set(newToken, forKey: "bearerToken")
-//        }
-//    }
-//}
-
 final class OAuth2TokenStorage {
     static let shared = OAuth2TokenStorage()
 
@@ -19,7 +8,7 @@ final class OAuth2TokenStorage {
 
     var token: String? {
         get {
-            return userDefaults.string(forKey: tokenKey)
+            userDefaults.string(forKey: tokenKey)
         }
         set {
             userDefaults.setValue(newValue, forKey: tokenKey)
