06349333caac02c2c29f3cfe00d86e77923cfb9f
Denis Beliakov <ddenn@outlook.com>
2023-06-13 14:41:32 +0300

    Доработан SingleImageVC

--- ImageFeed/Scenes/ImageList/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/Scenes/ImageList/SingleImageView/SingleImageViewController.swift
@@ -16,12 +16,16 @@ final class SingleImageViewController: UIViewController {
     }
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        scrollView.maximumZoomScale = 1.25
-        scrollView.minimumZoomScale = 0.1
-        scrollView.delegate = self
-        setImage(url: imageURL)
-    }
+            super.viewDidLoad()
+            setupScrollView()
+            setImage(url: imageURL)
+        }
+
+        private func setupScrollView() {
+            scrollView.maximumZoomScale = 1.25
+            scrollView.minimumZoomScale = 0.1
+            scrollView.delegate = self
+        }
 
     @IBAction private func didTapShareButton(_ sender: Any) {
         guard let image = imageView.image else { return }
@@ -32,37 +36,37 @@ final class SingleImageViewController: UIViewController {
     }
 
     func setImage(url: URL?) {
-        guard let url = url else { return }
-        UIBlockingProgressHUD.show()
-        imageView.kf.setImage(with: url) {[weak self] result in
-            UIBlockingProgressHUD.dismiss()
-            guard let self = self else { return }
-            switch result {
-            case .success(let resultImage):
-                self.rescaleAndCenterImageInScrollView(image: resultImage.image)
-                self.imageView.contentMode = .scaleAspectFill
-            case .failure(_):
-                self.showError()
+            guard let url = url else { return }
+            UIBlockingProgressHUD.show()
+            imageView.kf.setImage(with: url) { [weak self] result in
+                UIBlockingProgressHUD.dismiss()
+                guard let self = self else { return }
+                switch result {
+                case .success(let resultImage):
+                    self.rescaleAndCenterImageInScrollView(image: resultImage.image)
+                    self.imageView.contentMode = .scaleAspectFill
+                case .failure(_):
+                    self.showError()
+                }
             }
         }
-    }
 
-    private  func rescaleAndCenterImageInScrollView(image: UIImage) {
-        let minZoomScale = scrollView.minimumZoomScale
-        let maxZoomScale = scrollView.maximumZoomScale
-        view.layoutIfNeeded()
-        let visibleRectSize = scrollView.bounds.size
-        let imageSize = image.size
-        let hScale = visibleRectSize.width / imageSize.width
-        let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-        scrollView.setZoomScale(scale, animated: false)
-        scrollView.layoutIfNeeded()
-        let newContentSize = scrollView.contentSize
-        let xPoint = (newContentSize.width - visibleRectSize.width) / 2
-        let yPoint = (newContentSize.height - visibleRectSize.height) / 2
-        scrollView.setContentOffset(CGPoint(x: xPoint, y: yPoint), animated: false)
-    }
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+            let minZoomScale = scrollView.minimumZoomScale
+            let maxZoomScale = scrollView.maximumZoomScale
+            view.layoutIfNeeded()
+            let visibleRectSize = scrollView.bounds.size
+            let imageSize = image.size
+            let hScale = visibleRectSize.width / imageSize.width
+            let vScale = visibleRectSize.height / imageSize.height
+            let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+            scrollView.setZoomScale(scale, animated: false)
+            scrollView.layoutIfNeeded()
+            let newContentSize = scrollView.contentSize
+            let xPoint = (newContentSize.width - visibleRectSize.width) / 2
+            let yPoint = (newContentSize.height - visibleRectSize.height) / 2
+            scrollView.setContentOffset(CGPoint(x: xPoint, y: yPoint), animated: false)
+        }
 }
 
 extension SingleImageViewController: UIScrollViewDelegate {
