db9dcea7baf7093676c3c58b55242f9cb8d87b9f
Denis Beliakov <ddenn@outlook.com>
2023-06-14 12:54:17 +0300

    мелкие изменения

--- ImageFeed/Scenes/Login/AuthConfiguration.swift
+++ ImageFeed/Scenes/Login/AuthConfiguration.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-let unsplashAccessKey = "HnecKRVkkezqur4UPQdZtMwOLovEGMJH_fhUsFT8I5E"
-let unsplashSecretKey = "PSMlx_vYdr992nYwwZIarCoXcHYCveNZ7eeVR1QM6_w"
+let unsplashAccessKey = "b6CX1RCFR51uWh3lf369p6iPaE8y81Dzvy-_a9m2eNI"
+let unsplashSecretKey = "4iwE1TjomLp3InJoIZj2LvlOv-0Hq96VSbn_bvJy-kQ"
 let unsplashRedirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let unsplashAccessScope = "public+read_user+write_likes"
 let profilePath = "me"
