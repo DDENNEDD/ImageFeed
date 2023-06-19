2b9723e807f048c950516ab17e1ab24889d28571
Denis Beliakov <ddenn@outlook.com>
2023-06-19 05:16:25 +0300

    мелкие правки

--- ImageFeedTests/ImageListTest/ImageListTests.swift
+++ ImageFeedTests/ImageListTest/ImageListTests.swift
@@ -4,21 +4,17 @@ import XCTest
 final class ImageListTests: XCTestCase {
 
     func testSetLike () {
-
         let photos: [Photo] = []
         let imagesListService = ImagesListService.shared
         let view = ImageListViewControllerSpy(photos: photos)
         let presenter = ImageListPresenterSpy(imagesListService: imagesListService)
         view.presenter = presenter
         presenter.view = view
-
         view.setLike()
-
         XCTAssertTrue(presenter.didSetLikeCallSuccess)
     }
 
     func testLoadPhotoToTable1() {
-
         let tableView = UITableView()
         let tableCell = UITableViewCell()
         let indexPath: IndexPath = IndexPath(row: 2, section: 2)
@@ -28,9 +24,7 @@ final class ImageListTests: XCTestCase {
         let presenter = ImageListPresenterSpy(imagesListService: imagesListService)
         view.presenter = presenter
         presenter.view = view
-
         view.tableView(tableView, willDisplay: tableCell, forRowAt: indexPath)
-
         XCTAssertTrue(presenter.didFetchPhotosCalled)
     }
 }
